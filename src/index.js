50ec66e9c65b626dc765625f05faa452e6d15278
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2026-03-23 20:36:05 +0500

    Refactor server setup in index.js to implement Express and define a basic route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config({
   path: "./.env",
 });
 
-console.log("Hello, world");
-console.log("Environment Variables:", process.env.name);
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
