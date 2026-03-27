e53b285bcb1a7e834d2f3c0e4dea92d059080fb0
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2026-03-27 05:49:18 +0500

    Add method to generate refresh token using jsonwebtoken

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -84,4 +84,17 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiredIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
