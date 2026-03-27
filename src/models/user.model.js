86b2d9729cc2cbec4e30b74dea6f10a7694b65e8
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2026-03-27 05:48:45 +0500

    Add method to generate access token using jsonwebtoken

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -68,6 +69,19 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiredIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
 };
 
 export const User = mongoose.model("User", userSchema);
