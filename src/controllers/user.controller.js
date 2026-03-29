f9159a1803d4a60a9d81edcba666246711fee8b0
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2026-03-29 07:12:37 +0500

    Import emailVerificationMailGenContent function in user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { sendEmail } from "../utils/mail.js";
+import { emailVerificationMailGenContent } from "../utils/mail.js";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
