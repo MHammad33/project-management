db4c446aeb8ba787f93c43922e3360713c5d9f43
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2026-03-29 06:48:37 +0500

    Implement user registration and token generation with email verification

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,75 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { sendEmail } from "../utils/mail.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Error generating tokens");
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  const existingUser = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (existingUser) {
+    throw new ApiError(400, "Email or username already exists");
+  }
+
+  const user = await User.create({
+    email,
+    username,
+    password,
+    role,
+    isEmailVerfied: false,
+  });
+
+  const { unhashedToken, hashedToken, tokenExpiry } =
+    user.generateTemperaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Email Verification",
+    mailgenContent: emailVerificationMailGenContent(
+      user?.username,
+      `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unhashedToken}`,
+    ),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshtoken -emailVerificationToken -emailVerificationExpiry",
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Error registering user");
+  }
+
+  res.status(201).json(
+    new ApiResponse(
+      201,
+      {
+        user: createdUser,
+      },
+      "User registered successfully",
+    ),
+  );
+});
