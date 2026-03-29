8f4d5fd6b0082ea2d655697e1cc608aa6a47a993
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2026-03-29 06:51:49 +0500

    Add authentication routes to the application

--- src/app.js
+++ src/app.js
@@ -20,8 +20,10 @@ app.use(
 
 // Import Routes
 import healthCheckRouter from "./routes/healthcheck.route.js";
+import authRouter from "./routes/auth.routes.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
