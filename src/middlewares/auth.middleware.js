5f4ff47a19d014fd72023a07915728a9f13860f7
Muhammad Hammad Afzal <mhammadafzal3333@gmail.com>
2026-04-02 09:51:23 +0500

    Reorder imports in auth middleware for consistency

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
-import { asyncHandler } from "../utils/async-handler.js";
-import { ApiError } from "../utils/api-error.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   const token =
